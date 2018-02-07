afc46c2dc63dcdd23e58e95c0530d8186400564e
Darren Bellew <islarf6546@gmail.com>
2018-02-07 15:03:02 +0000

    commit message here

--- Problem 10/sieve_of_eratosthenes.py
+++ Problem 10/sieve_of_eratosthenes.py
@@ -1,7 +1,2 @@
 #intro
-print("The sieve of Eratothenes - by Darren Bellew")
-
-
-
-destination = input("Number to sieve primes: ")
-print(destination)
+print("The sieve of Eratothenes - by Darren Bellew") 
