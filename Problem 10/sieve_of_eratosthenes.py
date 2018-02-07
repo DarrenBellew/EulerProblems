f57563833d709553f7d9cb2758c82a7a24c680db
Darren Bellew <islarf6546@gmail.com>
2018-02-07 15:38:37 +0000

    sieve basics

--- Problem 10/sieve_of_eratosthenes.py
+++ Problem 10/sieve_of_eratosthenes.py
@@ -1,2 +1,17 @@
 #intro
-print("The sieve of Eratothenes - by Darren Bellew") 
+print("The sieve of Eratothenes - by Darren Bellew")
+
+#sievenum = input("Number to sive primes for: ")
+sievenum = 23
+
+sieve = [x for x in range(2,sievenum)]
+p=2
+
+sieve_notprime = sieve[p-2::p]
+
+print(str(sieve_notprime))
+
+p=5
+sieve_notprime.extend(sieve[p-2::p])
+
+print(str(sieve_notprime))
